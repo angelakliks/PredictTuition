7e3e9d571b4cffb450eedbccd163ddea56dd5dc8
Angela <angela.kliks@gmail.com>
2021-02-23 18:38:45 -0500

    Initial Commit

---
+++ predict tuition/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  predict tuition
+//
+//  Created by Angela Kliks on 2/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
